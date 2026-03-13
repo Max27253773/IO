afd2aabde8e960f68dfec1c79877fd7e4b398ccc
Max27253773 <maxwade370@gmail.com>
2026-03-13 10:55:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ from PIL import Image, ImageDraw, ImageFont
 st.sidebar.markdown(
     """
     <div style="background: linear-gradient(90deg, #0026C7 0%, #FFFFFF 40%, #FFFFFF 60%, #C70000 100%); 
-                padding: 1px; border-radius: 3px; text-align: center; 
+                padding: 3px; border-radius: 3px; text-align: center; 
                 width: 50%; margin: 0 auto;">
             <p style="font-size: 9px !important; color: black; margin: 0; letter-spacing: 1px; text-transform: uppercase; font-family: 'Impact';">
-                🌌 EROS
+                🌌 IO
             </p>
         </div>
     </div>
