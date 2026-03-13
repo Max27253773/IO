6063750314a7069b21c6b75a9e681f6da4815d18
Max27253773 <maxwade370@gmail.com>
2026-03-13 10:35:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ st.sidebar.markdown(
     """
     <div style="background: linear-gradient(90deg, #0026C7 0%, #FFFFFF 40%, #FFFFFF 60%, #C70000 100%); 
                 padding: 1px; border-radius: 3px; text-align: center; 
-                width: 80%; margin: 0 auto;">
-            <p style="font-size: 5px !important; color: black; margin: 0; letter-spacing: 1.5px; text-transform: uppercase; font-family: 'Impact';">
+                width: 100%; margin: 0 auto;">
+            <p style="font-size: 5px !important; color: black; margin: 0; letter-spacing: 1px; text-transform: uppercase; font-family: 'Impact';">
                 🌌 L'INTERSTELLAR
             </p>
         </div>
